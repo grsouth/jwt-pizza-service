e644e1128feedd961692cd6c4a51cfe67fd1d4da
grsouth <garrettsoutham@proton.me>
2024-12-11 23:45:09 -0700

    grafana real

--- src/metrics.js
+++ src/metrics.js
@@ -154,6 +154,32 @@ function sendMetricsPeriodically(period) {
     }, period);
   }
 
+class Metrics {
+  constructor() {
+    this.config = require('./config.json');
+  }
+
+  sendMetricToGrafana(metricPrefix, httpMethod, metricName, metricValue) {
+    const metric = `${metricPrefix},source=${this.config.source},method=${httpMethod} ${metricName}=${metricValue}`;
+
+    fetch(`${this.config.url}`, {
+      method: 'post',
+      body: metric,
+      headers: { Authorization: `Bearer ${this.config.userId}:${this.config.apiKey}` },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error('Failed to push metrics data to Grafana');
+        } else {
+          console.log(`Pushed ${metric}`);
+        }
+      })
+      .catch((error) => {
+        console.error('Error pushing metrics:', error);
+      });
+  }
+}
+
 module.exports = {
   requestTracker,
   authTracker,
@@ -163,4 +189,5 @@ module.exports = {
   getMetrics,
   resetMetrics,
   sendMetricsPeriodically,
+  Metrics,
 };
